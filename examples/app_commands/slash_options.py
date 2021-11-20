96cdee4544f3bbb873620ba7a8926d6f7dc5a672
ThatGenZGamer48 <78960084+ThatGenZGamer48@users.noreply.github.com>
2021-11-20 14:42:32 +0530

    Update slash_options.py

--- examples/app_commands/slash_options.py
+++ examples/app_commands/slash_options.py
@@ -7,6 +7,8 @@ bot = discord.Bot()
 # slash commands. You can use @bot.slash_command with discord.Bot as well
 
 
+# Example of a slash command using Option type-hints.
+
 @bot.slash_command(guild_ids=[...])
 async def hello(
     ctx,
@@ -14,7 +16,17 @@ async def hello(
     gender: Option(str, "Choose your gender", choices=["Male", "Female", "Other"]),
     age: Option(int, "Enter your age", required=False, default=18),
 ):
-    await ctx.respond(f"Hello {name}")
+    await ctx.respond(f"Hello {name}, you are {age} years old and you gender is {gender}!")
 
+    
+# Example of a slash command using Option decorators.
+
+@bot.slash_command(guild_ids=[...])
+@discord.option("name", "Enter your name")
+@discord.option("gender", "Choose your gender", choices=["Male", "Female", "Other"])
+@discord.option("age", "Enter your age", required=False, default=18)
+async def hello(ctx, name: str, gender: str, age: int):
+    await ctx.respond(f"Hello {name}, you are {age} years old and you gender is {gender}!")
+    
 
 bot.run("TOKEN")
